2c99e4709336a211caa691b17fbdb4c2f0e6a3c8
Antonio <antonio.junior@original.io>
2024-07-29 16:06:29 -0300

    Displays all registers if Todos status

--- src/app/template/Register.tsx
+++ src/app/template/Register.tsx
@@ -12,7 +12,7 @@ export default function Register() {
   const { status } = useContext(GeneralContext);
 
   const [registers, setRegisters] = useState<Register[]>([]);
-  const [filteredRegisters, setFilteredRegisters] = useState<any>([]);
+  //const [filteredRegisters, setFilteredRegisters] = useState<any>([]);
 
   useEffect(() => {
     loadRegisters();
@@ -25,44 +25,49 @@ export default function Register() {
 
   async function filterRegisters(status: string) {
     const filteredRegs = await getRegistersByStatus(status);
-    setFilteredRegisters(filteredRegs);
+    setRegisters(filteredRegs);
   }
 
   useEffect(() => {
-    filterRegisters(status);
+    if (status === "Todos") {
+      loadRegisters();
+    } else {
+      filterRegisters(status);
+    }
   }, [status]);
 
   return (
     <div id="table" className="flex flex-col w-[65%]">
-      {filteredRegisters.map((reg: any) => (
-        <Link key={reg.id} href={{ pathname: "/dashboard", query: { id: reg.id, date: reg.date, type: reg.type, description: reg.description, value: reg.value, status: reg.status } }} className="w-full flex justify-between bg-slate-800 rounded py-4 px-2 mb-4">
-          <div id="left-infos" className="flex w-2/5 justify-between">
-            <span id="id" className="text-white font-bold">{reg.id}</span>
-            <span id="data" className="text-slate-400">{reg.date}</span>
-            <span id="descricao" className="text-white font-bold">{reg.type}</span>
-          </div>
-          <div id="right-infos" className="flex w-2/5 justify-between">
-            <span id="valor" className="text-white font-bold flex justify-between w-2/5">
+      {
+        registers.map((reg: any) => (
+          <Link key={reg.id} href={{ pathname: "/dashboard", query: { id: reg.id, date: reg.date, type: reg.type, description: reg.description, value: reg.value, status: reg.status } }} className="w-full flex justify-between bg-slate-800 rounded py-4 px-2 mb-4">
+            <div id="left-infos" className="flex w-2/5 justify-between">
+              <span id="id" className="text-white font-bold">{reg.id}</span>
+              <span id="data" className="text-slate-400">{reg.date}</span>
+              <span id="descricao" className="text-white font-bold">{reg.type}</span>
+            </div>
+            <div id="right-infos" className="flex w-2/5 justify-between">
+              <span id="valor" className="text-white font-bold flex justify-between w-2/5">
+                {
+                  reg.type === "Receita"
+                    ?
+                    <span id="tipo" className="text-green-500"><IconTrendingUp stroke={2} /></span>
+                    :
+                    <span id="tipo" className="text-red-500"><IconTrendingDown stroke={2} /></span>
+                }
+                {reg.value}
+              </span>
               {
-                reg.type === "Receita"
+                reg.status.charAt(0).toUpperCase() + reg.status.slice(1) === "Consolidado"
                   ?
-                  <span id="tipo" className="text-green-500"><IconTrendingUp stroke={2} /></span>
+                  <span id="status" className="text-green-500 flex w-2/5 items-center justify-evenly"><IconCircleFilled size={16} />  {reg.status}</span>
                   :
-                  <span id="tipo" className="text-red-500"><IconTrendingDown stroke={2} /></span>
+                  <span id="status" className="text-red-500 flex w-2/5 items-center justify-evenly"><IconCircleFilled size={16} />  {reg.status}</span>
               }
-              {reg.value}
-            </span>
-            {
-              reg.status.charAt(0).toUpperCase() + reg.status.slice(1) === "Consolidado"
-                ?
-                <span id="status" className="text-green-500 flex w-2/5 items-center justify-evenly"><IconCircleFilled size={16} />  {reg.status}</span>
-                :
-                <span id="status" className="text-red-500 flex w-2/5 items-center justify-evenly"><IconCircleFilled size={16} />  {reg.status}</span>
-            }
-            <span className="text-white"><IconChevronRight stroke={1} /></span>
-          </div>
-        </Link>
-      ))}
+              <span className="text-white"><IconChevronRight stroke={1} /></span>
+            </div>
+          </Link>
+        ))}
     </div>
   )
 }
