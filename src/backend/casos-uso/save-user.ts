eb80fc3b7a4c91a77c326bb39f430a654adcd84e
Antonio <antonio.junior@original.io>
2024-08-03 21:16:24 -0300

    Login & Sign up pages

--- src/backend/casos-uso/save-user.ts
+++ src/backend/casos-uso/save-user.ts
@@ -1,9 +1,6 @@
 "use server"
 
-import Id from "@/core/utils/id";
 import RepositorioTarefa from "../db/RepositorioTarefa";
-import { User } from "@/core/model/user";
-
 
 export default async function saveUser(user: any) {
   const repo = new RepositorioTarefa();
